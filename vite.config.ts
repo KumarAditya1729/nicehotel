4313b38ee235e29b10e9aeaea72ed22ea366c979
adityashrivastavaaaaaaa-lang <arkashrios@gmail.com>
2026-07-01 15:48:52 +0530

    Configure vercel deployment with api route and rewrites

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+;
 
 export default defineConfig({
   plugins: [tanstackStart(), react(), tsConfigPaths(), tailwindcss()],
